01dcd76cd861620b9ccd5170eb44c9b398d155a9
M. Hisyam Ahmad Hasan Hazmi <hisyamhazmi26@gmail.com>
2022-12-12 17:02:16 -1200

    TUGAS sesi 10

---
+++ Tugas Sesi 10 Dasprog.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[26]:
+
+
+#Soal No 1
+list1 = [456,700,200,1000,2000]
+
+print(max(list1))
+
+print('\n')
+
+list1 = [456,700,200,1000,2000]
+
+print(min(list1))
+
+print('\n')
+
+list1= ['a','e','i','u']
+list1.insert(0,'o')
+print(list1)
+
+
+# In[ ]:
+
+
+
+
+
+# In[49]:
+
+
+#Soal No 2
+
+for x in ('g','a','n','t','e','n','g') : print (x, end = ' ')
+
+print('\n')
+
+tup1=[0,200,1000,2,3,9000,10]
+print(max(tup1))
+
+print('\n')
+
+tup1=[0,200,1000,2,3,9000,10]
+print(min(tup1))
+
+
+# In[52]:
+
+
+#Soal No 3
+
+dict1={100,212,41,4323,542,23,9}
+
+print(min(dict1))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
